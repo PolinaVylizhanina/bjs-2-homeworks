d17f17d708fd9703db219d3e1c372beeeb5d11ec
Polina Vylizhanina <vylizhaninaps@gmail.com>
2021-10-02 14:42:12 +0500

    task for object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,51 @@
-function Student(name, gender, age) {
-    // Ваш код
+function Student(name, gender, age) {   
+   
+    this.name = name;
+    this.gender = gender;
+    this.age = age; 
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  
+  if(this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+
+}
+
+Student.prototype.addMarks = function(...mark) {
+
+  for (let i = 0; i < mark.length; i++)
+    {  
+      this.marks.push(mark[i]);
+    }
+
+}
+
+Student.prototype.getAverage = function() {
+
+  let avg;
+  let sum = 0;
+  
+  for (let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i]
+  }
+
+  return avg = sum / this.marks.length;
+
+}
+
+Student.prototype.exclude = function(reason) {
+
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+
+}
