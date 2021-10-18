d7b0ceeb55902588b92a6e4eff8bf2068feb3a50
Polina Vylizhanina <vylizhaninaps@gmail.com>
2021-10-18 16:47:34 +0500

    decorators task 1 ,2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,11 +1,55 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  
+  let cash = [];
+
+  function wrapper(...args) {
+
+    let hash = args.join(",");
+    let indx = cash.findIndex(item => item.hash === hash);  
+    if (indx !== -1) {
+
+      console.log("Из кэша: " + cash[indx].value); 
+      return "Из кэша: " + cash[indx].value;
+
+    } else {
+
+      let result = func(...args);
+      cash.push({hash: hash, value: result});
+
+      if (cash.length > 5) { 
+        
+        cash.shift();
+
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;  
+   }
+
+  };
+
+  return wrapper;
+
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+function debounceDecoratorNew(func, ms) {  
+  
+  let flag = false;
+
+  return function(...args) {
+
+      if (flag) return;
+
+      func.apply(this, args);
+
+      flag = true;
+
+      setTimeout(() => flag = false, ms);
+  };
+
+}; 
+
+
 
 function debounceDecorator2(func) {
   // Ваш код
